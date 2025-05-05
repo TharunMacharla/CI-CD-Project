86ea8ca2f8984a3250865443c5e1b30b0e4721d1
TharunMacharla <tharunmacharla14@gmail.com>
2025-05-05 15:55:58 +0530

    inital commit/ci-cd1

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-# python
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-return "Hello from CI/CD app on Local VM!"
-if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5000)
+    return "Hello from CI/CD app on Local VM!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
